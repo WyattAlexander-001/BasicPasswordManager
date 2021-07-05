250e0aafc3a3242b200abd2d7f28ff36ada8a2dd
WyattAlexander-001 <wyattbushman117@gmail.com>
2021-07-05 14:40:24 -0400

    Update BasicPasswordManager.py

--- BasicPasswordManager.py
+++ BasicPasswordManager.py
@@ -1,25 +1,54 @@
-# -*- coding: utf-8 -*-
-"""
-Basic Password Bank
 """
 
-pwd= input('What is the master password?')
+Basic Password Bank
+
+*Need pip install cryptography
+
+"""
+
+#module to encrpyt text
+from cryptography.fernet import Fernet
+
+
+master_pwd= input('What is the master password?\n')
+
+
+
+
+#Functions:
+    
+def write_key():
+    key = Fernet.generate_key()
+    with open('key.key', 'wb') as key_file:
+        key_file.write(key)
 
 def view():
-    pass
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            #rstrip removes the \n from the add
+            data = line.rstrip()
+            #user is 0 index, passw is 1 index
+            user, passw = data.split('|')
+            print("User:", user, '|| Password:', passw)
+    
 
 def add():
-    pass
-
+    name = input('Account Name:\n')
+    pwd = input("Password:\n")
+    
+    #with will manually close the txt file a = append, r = read, w = write
+    with open('passwords.txt', 'a') as f:
+        f.write(name + '|' + pwd + '\n' )
+    
 while True:
-    mode = input("ADD a new password or VIEW existing password(s) (add,view), press q to quit?").lower()
+    mode = input("ADD a new password or VIEW existing password(s) (add,view), press q to quit?\n").lower()
     if mode == 'q':
         break
         
     if mode == 'view':
-        pass
+        view()
     elif mode == 'add':
-        pass
+        add()
     else:
         print('Invalid Input.')
         continue
